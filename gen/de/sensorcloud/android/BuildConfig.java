a3a9c34a1c4d30e5407980919210d50c81e2245a
Arthur Schneider <nc-schneiar15@netcologne.de>
2013-11-09 17:13:21 +0100

    Initialisierung des Projekts

---
+++ gen/de/sensorcloud/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.sensorcloud.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/de/sensorcloud/android/entitaet/NutzerEmail.java
@@ -0,0 +1,43 @@
+package de.sensorcloud.android.entitaet;
+
+public class NutzerEmail {
+
+	private String nutEmaID;
+	private String nutEmaNutStaID;
+	private String nutEmaAdr;
+	private String nutEmaBez;
+
+	
+	public String getNutEmaID() {
+		return nutEmaID;
+	}
+
+	public void setNutEmaID(String nutEmaID) {
+		this.nutEmaID = nutEmaID;
+	}
+
+	public String getNutEmaNutStaID() {
+		return nutEmaNutStaID;
+	}
+
+	public void setNutEmaNutStaID(String nutEmaNutStaID) {
+		this.nutEmaNutStaID = nutEmaNutStaID;
+	}
+
+	public String getNutEmaAdr() {
+		return nutEmaAdr;
+	}
+
+	public void setNutEmaAdr(String nutEmaAdr) {
+		this.nutEmaAdr = nutEmaAdr;
+	}
+
+	public String getNutEmaBez() {
+		return nutEmaBez;
+	}
+
+	public void setNutEmaBez(String nutEmaBez) {
+		this.nutEmaBez = nutEmaBez;
+	}
+
+}
