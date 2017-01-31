4877b3f79b4f57997f9cec2b8303f3d5e8fb96c8
Arthur Schneider <dev.schneiart@gmail.com>
2017-01-31 22:05:36 +0100

    Update Helper.java

--- src/de/sensorcloud/android/helpertools/Helper.java
+++ src/de/sensorcloud/android/helpertools/Helper.java
@@ -4,8 +4,7 @@ import java.util.UUID;
 
 public class Helper {
 
-//	public static String BASE_URL = "http://10.0.2.2:8080";
-	public static String BASE_URL = "http://babeauf.nt.fh-koeln.de:8080";
+	public static String BASE_URL = "http://10.0.2.2:8080";
 	
 	public static String generateUUID(){
 		return UUID.randomUUID().toString();
